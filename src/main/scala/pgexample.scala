a06aa3ce541984b4570ecdbdaf6a0b4fe36adad8
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-04-01 12:12:23 -0400

    Demonstrate failure with SAVEPOINT
    ```
    [error] (run-main-0) com.twitter.finagle.ChannelWriteException: com.twitter.finagle.UnknownChannelException: Unknown event com.twitter.finagle.postgres.messages.Sync$@32f0b05e for state ExecutePreparedStatement at remote address: localhost/127.0.0.1:26257 from service: client
    com.twitter.finagle.ChannelWriteException: com.twitter.finagle.UnknownChannelException: Unknown event com.twitter.finagle.postgres.messages.Sync$@32f0b05e for state ExecutePreparedStatement at remote address: localhost/127.0.0.1:26257 from service: client
    ```

--- src/main/scala/pgexample.scala
+++ src/main/scala/pgexample.scala
@@ -1,9 +1,35 @@
+import com.twitter.conversions.time._
+import com.twitter.util.Awaitable._
 import com.twitter.finagle.postgres._;
+import com.twitter.finagle.postgres.codec.ServerError
+
+package object pgexample {
+  def cockroachTxn(client: Client, from: Int, to: Int, amount: Int) {
+    val attempt = () => {
+      println("b")
+      val q = for {
+        _ <- client.prepareAndExecute("begin")
+        // Works unless this one is activated (regardless of whether the RELEASE
+        // below is).
+        _ <- client.prepareAndExecute("SAVEPOINT cockroach_restart")
+        _ <- client.prepareAndExecute("UPDATE accounts SET balance=balance+$2 WHERE id=$1",
+              to, amount)
+        _ <- client.prepareAndExecute("UPDATE accounts SET balance=balance+$2 WHERE id=$1",
+              from, -amount)
+        _ <- client.prepareAndExecute("RELEASE SAVEPOINT cockroach_restart")
+      } yield client.prepareAndExecute("commit")
+
+      q.get()
+    }
+    attempt()
+  }
+}
 
 object PGExample {
   def main(args: Array[String]) = {
-    val client = Client("localhost:26257", "root", null, "")
-    val f = client.select("select * from system.lease") { row => row }
-    println("Responded " + f.get)
+    val client = Client("localhost:26257", "root", null, "bank")
+    client.prepareAndExecute("CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)").get()
+    pgexample.cockroachTxn(client, 1, 2, 13)
+    val f = client.select("select id, balance from accounts;") { row => println(row.get("id").toString + ": " + row.get("balance").toString) }
   }
 }
